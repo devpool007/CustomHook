a1a75ed925e95c54079f8722a157808c3cdc897f
devpool007 <sharmadevansh007777@gmail.com>
2025-05-19 19:42:04 +0200

    made the custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,25 @@
-import { useRef, useState, useCallback, useEffect } from 'react';
-
-import Places from './components/Places.jsx';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
-import AvailablePlaces from './components/AvailablePlaces.jsx';
-import { fetchUserPlaces, updateUserPlaces } from './http.js';
-import Error from './components/Error.jsx';
+import { useRef, useState, useCallback, useEffect } from "react";
+import Places from "./components/Places.jsx";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import AvailablePlaces from "./components/AvailablePlaces.jsx";
+import { fetchUserPlaces, updateUserPlaces } from "./http.js";
+import Error from "./components/Error.jsx";
+import { useFetch } from "./hooks/useFetch.js";
+import { use } from "react";
 
 function App() {
   const selectedPlace = useRef();
 
-  const [userPlaces, setUserPlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
-
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
-
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        const places = await fetchUserPlaces();
-        setUserPlaces(places);
-      } catch (error) {
-        setError({ message: error.message || 'Failed to fetch user places.' });
-      }
-
-      setIsFetching(false);
-    }
-
-    fetchPlaces();
-  }, []);
+  const {
+    isFetching,
+    fetchedData: userPlaces,
+    error,
+  } = useFetch(fetchUserPlaces, []);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -44,52 +30,50 @@ function App() {
     setModalIsOpen(false);
   }
 
-  async function handleSelectPlace(selectedPlace) {
-    // await updateUserPlaces([selectedPlace, ...userPlaces]);
+  // async function handleSelectPlace(selectedPlace) {
+  //   // await updateUserPlaces([selectedPlace, ...userPlaces]);
+  //   //   setUserPlaces((prevPickedPlaces) => {
+  //   //     if (!prevPickedPlaces) {
+  //   //       prevPickedPlaces = [];
+  //   //     }
+  //   //     if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
+  //   //       return prevPickedPlaces;
+  //   //     }
+  //   //     return [selectedPlace, ...prevPickedPlaces];
+  //   //   });
+  //   //   try {
+  //   //     await updateUserPlaces([selectedPlace, ...userPlaces]);
+  //   //   } catch (error) {
+  //   //     setUserPlaces(userPlaces);
+  //   //     setErrorUpdatingPlaces({
+  //   //       message: error.message || 'Failed to update places.',
+  //   //     });
+  //   //   }
+  // }
 
-    setUserPlaces((prevPickedPlaces) => {
-      if (!prevPickedPlaces) {
-        prevPickedPlaces = [];
-      }
-      if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
-        return prevPickedPlaces;
-      }
-      return [selectedPlace, ...prevPickedPlaces];
-    });
+  // const handleRemovePlace = useCallback(
+  //   async function handleRemovePlace() {
+  //     // setUserPlaces((prevPickedPlaces) =>
+  //     //   prevPickedPlaces.filter(
+  //     //     (place) => place.id !== selectedPlace.current.id
+  //     //   )
+  //     // );
 
-    try {
-      await updateUserPlaces([selectedPlace, ...userPlaces]);
-    } catch (error) {
-      setUserPlaces(userPlaces);
-      setErrorUpdatingPlaces({
-        message: error.message || 'Failed to update places.',
-      });
-    }
-  }
+  //     // try {
+  //     //   await updateUserPlaces(
+  //     //     userPlaces.filter((place) => place.id !== selectedPlace.current.id)
+  //     //   );
+  //     // } catch (error) {
+  //     //   setUserPlaces(userPlaces);
+  //     //   setErrorUpdatingPlaces({
+  //     //     message: error.message || 'Failed to delete place.',
+  //     //   });
+  //     // }
 
-  const handleRemovePlace = useCallback(
-    async function handleRemovePlace() {
-      setUserPlaces((prevPickedPlaces) =>
-        prevPickedPlaces.filter(
-          (place) => place.id !== selectedPlace.current.id
-        )
-      );
-
-      try {
-        await updateUserPlaces(
-          userPlaces.filter((place) => place.id !== selectedPlace.current.id)
-        );
-      } catch (error) {
-        setUserPlaces(userPlaces);
-        setErrorUpdatingPlaces({
-          message: error.message || 'Failed to delete place.',
-        });
-      }
-
-      setModalIsOpen(false);
-    },
-    [userPlaces]
-  );
+  //     setModalIsOpen(false);
+  //   },
+  //   [userPlaces]
+  // );
 
   function handleError() {
     setErrorUpdatingPlaces(null);
@@ -110,7 +94,7 @@ function App() {
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
+          // onConfirm={handleRemovePlace}
         />
       </Modal>
 
@@ -135,7 +119,9 @@ function App() {
           />
         )}
 
-        <AvailablePlaces onSelectPlace={handleSelectPlace} />
+        <AvailablePlaces 
+        // onSelectPlace={handleSelectPlace} 
+        />
       </main>
     </>
   );
